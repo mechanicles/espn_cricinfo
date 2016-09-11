2d9e4c17677b544d20a87cf42ac7697bca11a4ab
Santosh Wadghule <santosh.wadghule@gmail.com>
2016-09-11 13:41:55 +0530

    Eager load files under lib folder.

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,6 @@ module EspnCricinfo
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.autoload_paths << "#{Rails.root}/lib"
+    config.eager_load_paths << "#{Rails.root}/lib"
   end
 end
