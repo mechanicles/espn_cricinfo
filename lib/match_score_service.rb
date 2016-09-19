d98b5fa3cef0622d314852a7a98da49d0254d1c4
Santosh Wadghule <santosh.wadghule@gmail.com>
2016-09-19 13:10:11 +0530

    Use direct exchange type instead of fanout.
    Fanout exchange sends the messages to all its connected queues.

--- lib/match_score_service.rb
+++ lib/match_score_service.rb
@@ -10,7 +10,7 @@ class MatchScoreService
   def publish
     return if match.nil?
 
-    exchange = channel.fanout("match_scores")
+    exchange = channel.direct("match_scores")
     exchange.publish(payload, routing_key: queue.name, persistent: true)
     connection.close
   end
